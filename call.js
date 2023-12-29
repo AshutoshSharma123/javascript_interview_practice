a42cd9a1be5186429c89f189723e59bef251ecec
AshutoshSharma123 <aashusharma375@gmail.com>
2023-12-29 23:27:27 +0530

    apply method

--- call.js
+++ call.js
@@ -1,7 +1,7 @@
 // a function that takes a parameter 
-let printDetails = function (city) {
+let printDetails = function (city, village) {
 
-    console.log(`Hello i am ${this.name} and i am ${this.age} years old, i am from ${city}`);
+    console.log(`Hello i am ${this.name} and i am ${this.age} years old, i am from ${city} and my village is ${village}`);
 }
 person1 = {
     name: 'John',
@@ -12,5 +12,11 @@ person2 = {
     name: 'nikki',
     age: 20
 }
-printDetails.call(person1, 'Jammu') // in universal call the object is taken forst and the the parameter i.e. the city name here
+printDetails.call(person1, 'Jammu', 'Bishnah') // in universal call the object is taken forst and the the parameter i.e. the city name here
+printDetails.call(person2, 'Jammu', 'Bishnah') // in universal call the object is taken forst and the the parameter i.e. the city name here
+printDetails.apply(person1, ['Jammu', 'Bishnah']) // in universal apply the object is taken forst and the the parameter i.e. the city name here but using arrays
+printDetails.apply(person2, ['Jammu', 'Bishnah']) // in universal apply the object is taken forst and the the parameter i.e. the city name here but using arrays
+
+
+
 // person1.printDetails.call(person2);
