bc4c2830606d797bef206e654e74a03a7641dfbb
AshutoshSharma123 <aashusharma375@gmail.com>
2023-12-29 23:32:36 +0530

    about bind

--- call.js
+++ call.js
@@ -12,11 +12,19 @@ person2 = {
     name: 'nikki',
     age: 20
 }
+// call 
 printDetails.call(person1, 'Jammu', 'Bishnah') // in universal call the object is taken forst and the the parameter i.e. the city name here
 printDetails.call(person2, 'Jammu', 'Bishnah') // in universal call the object is taken forst and the the parameter i.e. the city name here
+
+// apply
 printDetails.apply(person1, ['Jammu', 'Bishnah']) // in universal apply the object is taken forst and the the parameter i.e. the city name here but using arrays
 printDetails.apply(person2, ['Jammu', 'Bishnah']) // in universal apply the object is taken forst and the the parameter i.e. the city name here but using arrays
 
+// bind
+
+let a = printDetails.bind(person2, 'Jammu', 'Bishnah')
+console.log(a)
+a()
 
 
 // person1.printDetails.call(person2);
