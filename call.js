d1039ceb9790e30666984c529ee90aac8f00faf8
AshutoshSharma123 <aashusharma375@gmail.com>
2023-12-29 23:19:15 +0530

    a universal call

--- call.js
+++ call.js
@@ -1,15 +1,16 @@
+// a function that takes a parameter 
+let printDetails = function (city) {
+
+    console.log(`Hello i am ${this.name} and i am ${this.age} years old, i am from ${city}`);
+}
 person1 = {
     name: 'John',
     age: 21,
-    printDetails: function () {
 
-        console.log(`Hello i am ${this.name} and i am ${this.age} years old`);
-    }
 }
 person2 = {
     name: 'nikki',
     age: 20
 }
-
-person1.printDetails()
-person1.printDetails.call(person2);
+printDetails.call(person1, 'Jammu') // in universal call the object is taken forst and the the parameter i.e. the city name here
+// person1.printDetails.call(person2);
